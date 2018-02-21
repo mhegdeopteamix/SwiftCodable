8648017cb615bb2a5a7999348ded8d3bc0af7532
mhegde <mhegde@opteamix.com>
2018-02-21 11:26:32 +0530

    Intial commit

---
+++ SwiftCodable/SwiftCodable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCodable
+//
+//  Created by Mahabaleshwar Hegde on 21/02/18.
+//  Copyright © 2018 Mahabaleshwar Hegde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
